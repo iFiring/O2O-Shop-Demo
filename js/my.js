1e731342bf0b4e34095c5c9c48c10620962f3c7e
王川 <13170919480@163.com>
2017-11-28 01:28:41 +0800

    Add js files

---
+++ js/my.js
@@ -0,0 +1,374 @@
+//区域pop
+	$(function ($) {
+		//弹出选择框
+		$("#addrout").hover(function () {
+			$(this).stop().animate({opacity: '0.7'}, 500);
+		}, function () {
+			$(this).stop().animate({opacity: '1'}, 500);
+		}).on('click', function () {
+			$("body").append("<div id='mask'></div>");
+			$("#mask").addClass("mask").fadeIn("slow");
+			$(".AddrBox").fadeIn("slow");
+		});
+		//按钮的透明度
+		$("#addrsubmit").hover(function () {
+			$(this).stop().animate({opacity: '1'}, 600);
+		}, function () {
+			$(this).stop().animate({opacity: '0.8'}, 1000);
+		});
+		//文本框不允许为空---按钮触发//文本框不允许为空---单个文本触发
+		//关闭
+		$(".addrclose_btn").hover(function () { $(this).css({ color: '#FFF' }) }, function () { $(this).css({ color: '#CCC' }) }).on('click', function () {
+			$(".AddrBox").fadeOut("fast");
+			$("#mask").fadeOut("slow");
+		});
+	});
+//登录pop
+	$(function ($) {
+		//弹出登录
+		$("#usern").hover(function () {
+			$(this).stop().animate({opacity: '0.7'}, 500);
+		}, function () {
+			$(this).stop().animate({opacity: '1'}, 500);
+		}).on('click', function () {
+			$("body").append("<div id='mask'></div>");
+			$("#mask").addClass("mask").fadeIn("slow");
+			$(".login_Tabs").fadeIn("slow");
+		});
+		//文本框不允许为空---按钮触发//文本框不允许为空---单个文本触发
+		//关闭
+		$(".loginclose_btn").hover(function () { $(this).css({ color: '#FFF' }) }, function () { $(this).css({ color: '#CCC' }) }).on('click', function () {
+			$(".login_Tabs").fadeOut("fast");
+			$("#mask").fadeOut("slow");
+		});
+	});
+//*********************************************************************************************
+//loginTabs.js
+(function ($) {
+    $.fn.extend({
+        easyResponsiveTabs: function (options) {
+            //Set the default values, use comma to separate the settings, example:
+            var defaults = {
+                type: 'default', //default, vertical, accordion;
+                width: 'auto',
+                fit: true
+            }
+            //Variables
+            var options = $.extend(defaults, options);            
+            var opt = options, jtype = opt.type, jfit = opt.fit, jwidth = opt.width, vtabs = 'vertical', accord = 'accordion';
+
+            //Main function
+            this.each(function () {
+                var $respTabs = $(this);
+                $respTabs.find('ul.resp-tabs-list li').addClass('resp-tab-item');
+                $respTabs.css({
+                    'display': 'block',
+                    'width': 'auto'
+                });
+
+                $respTabs.find('.resp-tabs-container > div').addClass('resp-tab-content');
+                jtab_options();
+                //Properties Function
+                function jtab_options() {
+                    if (jtype == vtabs) {
+                        $respTabs.addClass('resp-vtabs');
+                    }
+                    if (jfit == true) {
+                        $respTabs.css({ width: '100%', margin: '0px' });
+                    }
+                    if (jtype == accord) {
+                        $respTabs.addClass('resp-easy-accordion');
+                        $respTabs.find('.resp-tabs-list').css('display', 'none');
+                    }
+                }
+
+                //Assigning the h2 markup
+                var $tabItemh2;
+                $respTabs.find('.resp-tab-content').before("<h2 class='resp-accordion' role='tab'><span class='resp-arrow'></span></h2>");
+
+                var itemCount = 0;
+                $respTabs.find('.resp-accordion').each(function () {
+                    $tabItemh2 = $(this);
+                    var innertext = $respTabs.find('.resp-tab-item:eq(' + itemCount + ')').text();
+                    $respTabs.find('.resp-accordion:eq(' + itemCount + ')').append(innertext);
+                    $tabItemh2.attr('aria-controls', 'tab_item-' + (itemCount));
+                    itemCount++;
+                });
+
+                //Assigning the 'aria-controls' to Tab items
+                var count = 0,
+                    $tabContent;
+                $respTabs.find('.resp-tab-item').each(function () {
+                    $tabItem = $(this);
+                    $tabItem.attr('aria-controls', 'tab_item-' + (count));
+                    $tabItem.attr('role', 'tab');
+
+                    //First active tab                   
+                    $respTabs.find('.resp-tab-item').first().addClass('resp-tab-active');
+                    $respTabs.find('.resp-accordion').first().addClass('resp-tab-active');
+                    $respTabs.find('.resp-tab-content').first().addClass('resp-tab-content-active').attr('style', 'display:block');
+
+                    //Assigning the 'aria-labelledby' attr to tab-content
+                    var tabcount = 0;
+                    $respTabs.find('.resp-tab-content').each(function () {
+                        $tabContent = $(this);
+                        $tabContent.attr('aria-labelledby', 'tab_item-' + (tabcount));
+                        tabcount++;
+                    });
+                    count++;
+                });
+
+                //Tab Click action function
+                $respTabs.find("[role=tab]").each(function () {
+                    var $currentTab = $(this);
+                    $currentTab.click(function () {
+
+                        var $tabAria = $currentTab.attr('aria-controls');
+
+                        if ($currentTab.hasClass('resp-accordion') && $currentTab.hasClass('resp-tab-active')) {
+                            $respTabs.find('.resp-tab-content-active').slideUp('', function () { $(this).addClass('resp-accordion-closed'); });
+                            $currentTab.removeClass('resp-tab-active');
+                            return false;
+                        }
+                        if (!$currentTab.hasClass('resp-tab-active') && $currentTab.hasClass('resp-accordion')) {
+                            $respTabs.find('.resp-tab-active').removeClass('resp-tab-active');
+                            $respTabs.find('.resp-tab-content-active').slideUp().removeClass('resp-tab-content-active resp-accordion-closed');
+                            $respTabs.find("[aria-controls=" + $tabAria + "]").addClass('resp-tab-active');
+
+                            $respTabs.find('.resp-tab-content[aria-labelledby = ' + $tabAria + ']').slideDown().addClass('resp-tab-content-active');
+                        } else {
+                            $respTabs.find('.resp-tab-active').removeClass('resp-tab-active');
+                            $respTabs.find('.resp-tab-content-active').removeAttr('style').removeClass('resp-tab-content-active').removeClass('resp-accordion-closed');
+                            $respTabs.find("[aria-controls=" + $tabAria + "]").addClass('resp-tab-active');
+                            $respTabs.find('.resp-tab-content[aria-labelledby = ' + $tabAria + ']').addClass('resp-tab-content-active').attr('style', 'display:block');
+                        }
+                    });
+                    //Window resize function                   
+                    $(window).resize(function () {
+                        $respTabs.find('.resp-accordion-closed').removeAttr('style');
+                    });
+                });
+            });
+        }
+    });
+})(jQuery);
+//Do easyResponsiveTabs()
+$(document).ready(function () {
+	$('.horizontalTab').easyResponsiveTabs({
+		type: 'default', //Types: default, vertical, accordion           
+		width: 'auto', //auto or any width like 600px
+		fit: true   // 100% fit in a container
+	});
+});
+//*********************************************************************************************
+//商铺pop //账户pop
+	<!--打开层--> 
+	function    locking(){
+	document.all.storepop.style.display='block'}
+	<!--关闭层--> 
+	function    Lock_CheckForm(theForm){
+	document.all.storepop.style.display='none';
+	return   false;}
+//*********************************************************************************************
+//tab标签切换
+$(function(){
+    function tabs(tabTit,on,tabCon){
+	$(tabCon).each(function(){
+	  $(this).children().eq(0).show();
+	  });
+	$(tabTit).each(function(){
+	  $(this).children().eq(0).addClass(on);
+	  });
+    $(tabTit).children().click(function(){
+      $(this).addClass(on).siblings().removeClass(on);
+      var index = $(tabTit).children().index(this);
+      $(tabCon).children().eq(index).show().siblings().hide();
+    });
+     }
+  tabs(".investment_title","on",".investment_con");
+ })
+//*********************************************************************************************
+//store筛选
+$(document).ready(function(){
+	$('.smenu div').click(function(){
+		$('.smenu').children('div').removeClass('on');
+		$(this).addClass('on');
+		thisItem= $(this).attr('rel');
+		if(thisItem != "all"){
+			$('.sitem div[rel='+thisItem+']').stop().animate({ 'opacity' : 1,width : '318px',marginLeft: '2px',marginTop:'2px'},200);
+			$('.sitem div[rel!='+thisItem+']').stop().animate({ 'opacity' : 0,width : 0,marginLeft: '0px',marginTop:'0px'},200);
+		}else{
+			$('.sitem div').stop().animate({'opacity' : 1,width : '318px',marginLeft: '2px',marginTop:'2px'},200);
+		};
+	});
+});
+//*********************************************************************************************
+//goods筛选
+	$(function () {
+		var filterList = {
+			init: function () {// MixItUp plugin
+				$('#portfoliolist').mixitup({targetSelector: '.portfolio',filterSelector: '.filter',effects: ['fade'],easing: 'snap',onMixEnd: filterList.hoverEffect()
+				});				
+			},
+			hoverEffect: function () {
+				$('#portfoliolist .portfolio').hover(
+					function () {
+						$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+						$(this).find('img').stop().animate({top: 0}, 500, 'easeOutQuad');},
+					function () {
+						$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+						$(this).find('img').stop().animate({top: 0}, 500, 'easeOutQuad');}
+			);}};
+	filterList.init();
+	});	
+//*********************************************************************************************
+//购物车goods增删改查
+$(document).ready(function () {
+	//jquery特效制作复选框全选反选取消(无插件)
+	// 全选        
+	$(".allselect").click(function () {
+		
+		if($(this).attr("checked")){
+			$(".gwc_tb2 input[name=newslist]").each(function () {
+				$(this).attr("checked", true);
+				// $(this).next().css({ "background-color": "#3366cc", "color": "#ffffff" });
+			});
+			GetCount();
+		
+		}
+		else
+   		{
+			$(".gwc_tb2 input[name=newslist]").each(function () {
+				if ($(this).attr("checked")) {
+					$(this).attr("checked", false);
+					//$(this).next().css({ "background-color": "#ffffff", "color": "#000000" });
+				} else {
+					$(this).attr("checked", true);
+					//$(this).next().css({ "background-color": "#3366cc", "color": "#000000" });
+				} 
+			});
+			GetCount();
+    
+   		}
+		
+	});
+
+	//反选
+	$("#invert").click(function () {
+		$(".gwc_tb2 input[name=newslist]").each(function () {
+			if ($(this).attr("checked")) {
+				$(this).attr("checked", false);
+				//$(this).next().css({ "background-color": "#ffffff", "color": "#000000" });
+			} else {
+				$(this).attr("checked", true);
+				//$(this).next().css({ "background-color": "#3366cc", "color": "#000000" });
+			} 
+		});
+		GetCount();
+	});
+
+	//取消
+	$("#cancel").click(function () {
+		$(".gwc_tb2 input[name=newslist]").each(function () {
+			$(this).attr("checked", false);
+			// $(this).next().css({ "background-color": "#ffffff", "color": "#000000" });
+		});
+		GetCount();
+	});
+
+	// 所有复选(:checkbox)框点击事件
+	$(".gwc_tb2 input[name=newslist]").click(function () {
+		if ($(this).attr("checked")) {
+			//$(this).next().css({ "background-color": "#3366cc", "color": "#ffffff" });
+		} else {
+			// $(this).next().css({ "background-color": "#ffffff", "color": "#000000" });
+		}
+		GetCount();
+	});
+
+	// 输出
+	$(".gwc_tb2 input[name=newslist]").click(function () {
+		// $("#total2").html() = GetCount($(this));
+		GetCount();
+	});
+});
+function GetCount() {
+	var conts = 0;
+	var aa = 0;
+	$(".gwc_tb2 input[name=newslist]").each(function () {
+		if ($(this).attr("checked")) {
+			for (var i = 0; i < $(this).length; i++) {
+				conts += parseInt($(this).val());
+				aa += 1;
+			}
+		}
+	});
+	$("#shuliang").text(aa);
+	$("#zong1").html((conts).toFixed(2));
+	if($("#zong1").text()>0){
+	$("#jz1").css("display", "none");
+	$("#jz2").css("display", "block");}
+	else{
+	$("#jz1").css("display", "block");
+	$("#jz2").css("display", "none");}
+}
+	<!---第一个商品加减算总数---->
+	$(function () {
+		var t = $("#text_box1");
+		$("#add1").click(function () {
+			t.val(parseInt(t.val()) + 1)
+			setTotal(); GetCount();
+		})
+		$("#min1").click(function () {
+			if(t.val()>1){
+				t.val(parseInt(t.val()) - 1)
+				setTotal(); GetCount();
+			}
+		})
+		function setTotal() {
+
+			$("#total1").html((parseInt(t.val()) * 9).toFixed(2));
+			$("#newslist-1").val(parseInt(t.val()) * 9);
+		}
+		setTotal();
+	})
+	<!---第二个商品加减算总数---->
+	$(function () {
+		var t = $("#text_box2");
+		$("#add2").click(function () {
+			t.val(parseInt(t.val()) + 1)
+			setTotal(); GetCount();
+		})
+		$("#min2").click(function () {
+			if(t.val()>1){
+				t.val(parseInt(t.val()) - 1)
+				setTotal(); GetCount();
+			}
+		})
+		function setTotal() {
+
+			$("#total2").html((parseInt(t.val()) * 8).toFixed(2));
+			$("#newslist-2").val(parseInt(t.val()) * 8);
+		}
+		setTotal();
+	})
+	<!---结算总数---->
+	$(function () {
+		$(".quanxun").click(function () {
+			setTotal();
+			//alert($(lens[0]).text());
+		});
+		function setTotal() {
+			var len = $(".tot");
+			var num = 0;
+			for (var i = 0; i < len.length; i++) {
+				num = parseInt(num) + parseInt($(len[i]).text());
+
+			}
+			//alert(len.length);
+			$("#zong1").text(parseInt(num).toFixed(2));
+			$("#shuliang").text(len.length);
+		}
+		setTotal();
+	})
+//*********************************************************************************************
